201cc5a251e83c34a7588d5f0e33e76d2846984b
Chris H <chris@makecoolstuff.net>
2021-09-03 10:06:43 +1000

    changed contacts list to display all users in graphql API

--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -5,13 +5,31 @@ import { View } from '../components/Themed';
 import chatRooms from '../data/ChatRooms';
 import NewMessageButton from '../components/NewMessageButton';
 import ContactListItem from '../components/ContactListItem';
-import Users from '../data/Users';
+import { useEffect, useState } from 'react';
+import { listUsers } from '../src/graphql/queries';
+import { API, graphqlOperation } from 'aws-amplify';
 
 export default function ContactsScreen() {
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+   const fetchUsers = async () => {
+     try {
+      const usersData = await API.graphql(graphqlOperation(listUsers))
+      console.log(usersData)
+      setUsers(usersData.data.listUsers.items)
+     } catch (e) {
+      console.log(e)
+     }
+   }
+   fetchUsers() 
+  }, [])
+
   return (
     <View style={styles.container}>
       <FlatList 
-        data={Users} 
+        data={users} 
         renderItem={({ item }) => <ContactListItem user={item}/>}
         keyExtractor={(item) => item.id}
         style={{width: '100%'}}
